5f5493d2205529678ac41f4f1de2c80ef5ab3514
MGApcDev <mgapcdev@gmail.com>
2016-05-30 00:41:34 +0200

    More testing

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,21 @@
+//var todo = document.registerElement('to-do', {
+//  prototype: Object.create(HTMLButtonElement.prototype),
+//  extends: 'div'
+//});
+//
+
+
 $(document).ready(function() {
   //$('.box').html('testing print');  
   //$('<style media="print">.box {color: green;}</style>').appendTo('head');
+  $('todo').css('margin-left', '10px');
+  var c = $('todo').html();
+  $('todo').html('<p>' + c + '</p>')
+  
+  
+  $('img').each(function() {
+    var cap = $(this).attr('caption');
+    if (cap != undefined)
+      $(this).after('<p class="caption">' + cap + '</p>')
+  });
 });
