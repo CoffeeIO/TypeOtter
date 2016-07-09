b841bd0f10c96761d3f3c4e1455622081150bbc9
MGApcDev <mgapcdev@gmail.com>
2016-07-09 15:52:31 +0200

    Check for empty elements

--- src/wrap.js
+++ src/wrap.js
@@ -5,12 +5,17 @@ function wrapper(elem) {
   //console.log('--> ' + elem.html());
   var html = ''; // returning markup
   var clone = elem.clone();
+  // Skip <p> and <script>
   if (clone.prop('tagName') == 'P') {
     return elem;
   }
   if (clone.prop('tagName') == 'SCRIPT') {
     return elem;
   }
+
+  //Check empty elements
+  if (clone.html().trim() == '') return elem;
+
   // Iterate over all children
   while (clone.children().length > 0) {
     if (regMarkup.test(clone.html()) == false) {
@@ -22,6 +27,7 @@ function wrapper(elem) {
     if (temp.html() !== undefined) html += temp.wrap('<div>').parent().html();
     clone.children(':nth-child(1)').remove();
   }
+
   if (regMarkup.test(clone.html()) == false) {
     clone.html('<p>' + clone.html());
     html += clone.children(':nth-child(1)').clone().wrap('<div>').parent().html();
