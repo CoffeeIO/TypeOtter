f9f25198a17fbbe4747e4ce3f171c1cff64f9052
MGApcDev <mgapcdev@gmail.com>
2016-07-11 02:43:12 +0200

    Testing element height

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,6 @@
-$(document).ready(function () {
+$(window).load(function () {
   console.log('------------------------------------');
+  var page = new Page();
 
+  console.log(texify(page, $('body')));
 });
--- src/scripts/page.js
+++ src/scripts/page.js
@@ -5,9 +5,24 @@ var Page = function () {
     this.html = ''
 };
 
+var curPage = 1;
+var totalPage = 1;
+
+var headerTemp = '<div class="header"></div>';
+var footerTemp = '<div class="footer"></div>';
+
+
 function makePage (pageSettings, html) {
+    console.log(html.children(':nth-child(1)').outerHeight(true));
+    console.log($('p:first-child').height());
     
 }
 function texify (pageSettings, html) {
-
+    var clone = html;
+    //while (clone.text().trim() != '') {
+        var obj = makePage(pageSettings, clone);
+        pageSettings.number = ++curPage;
+        //console.log(JSON.stringify(obj));
+    //}
+    return "hello";
 }
