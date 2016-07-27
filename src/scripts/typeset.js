276dad38a530c097259fe06b9457722c94ad5d10
MGApcDev <mgapcdev@gmail.com>
2016-07-28 01:37:04 +0200

    Implement render-based hyphenation

--- src/scripts/typeset.js
+++ src/scripts/typeset.js
@@ -1,83 +1,114 @@
-var splitPercent = 95;
+var splitPercent = 7;
 
-function rec(remain, carry, maxWidth, check, arrPos) {
-  if (arrPos >= remain.length) {
-    console.log('abort mission!')
-    return {
-      html: carry,
-      carry: null,
-      arrPos: null
+function rec(remain, lineHeight, dom) {
+    if (arrPos >= remain.length) {
+        console.log('abort mission!')
+        return {
+            html: carry,
+            carry: null,
+            arrPos: null
+        };
     }
-  }
   
-  check.html('');
-  if (carry !== null) {
-    check.html(carry);
-  }
-  
-  var temp = '', // temporary store for text pieces
-      minWidth = 0,
-      hyphenPos = 0;
-  while(check.width() <= maxWidth && remain[arrPos] != undefined) {
-      minWidth = check.width();
-      temp = check.html();
-      check.append(" " + remain[arrPos++]);
-  }
-
-  
-  var tempMax = check.width();
-  check.html(temp + ' ');
-  
-  console.log('min --> ' + minWidth);
-  console.log('max --> ' + tempMax);
-  
-
-
-  // Check if we should hyphenate.
-  if ((minWidth / maxWidth * 100) < splitPercent) {
-    console.log('begin hyphen');
-    console.log(minWidth + '-->' + tempMax);
-    var x = window['Hypher']['languages']['en-us'].hyphenate(remain[arrPos - 1]);
-    console.log(x);
+    dom.html('<span></span>');
     
-    
-    while(check.width() <= maxWidth && x[hyphenPos] != undefined) {
-      temp = check.html();
-      check.append(x[hyphenPos++]);
+  
+    var temp = '', // temporary store for text pieces
+        hyphenPos = 0,
+        line = 1,
+        arrPos = 0,
+        span = dom.find('span'),
+        carry = null;
+    while(remain[arrPos] != undefined) {
+        console.log('word  - %s, line - %s, span - ', remain[arrPos], line, span.html());
+        if (carry !== null) {
+            span.html(carry + ' ');
+            carry = null;
+        }
+        /*if (arrPos == 75) {
+            return;
+        }*/
+        
+        temp = span.html();
+        if (span.html() == '') span.append(remain[arrPos++]);
+        else span.append(" " + remain[arrPos++]);
+        
+        if (dom.height() > (lineHeight * line)) {
+            console.log('break');
+            span.html(temp + ' ');
+            
+            var minW = span.width(),
+                whitespace = getMaxWidth(dom, span, lineHeight * line),
+                maxW = minW + whitespace;
+            
+            console.log('comp mw: %s, dw: %s', minW, dom.width());
+            
+            console.log("min: %s, max: %s, split: %f", minW, maxW, (minW / maxW) * 100);
+                        
+            // Check if we should hyphenate.
+            if ((minW / maxW) * 100 < (100 - splitPercent)) {
+                hyphenPos = 0;
+                var x = window['Hypher']['languages']['en-us'].hyphenate(remain[arrPos - 1]);
+                console.log(x);
+                while (dom.height() <= (lineHeight * line) && x[hyphenPos] != undefined) {
+                  temp = span.html();
+                  span.append(x[hyphenPos++]);
+                }
+                if (hyphenPos == 1) {
+                  span.html(temp);
+                } else {
+                  span.html(temp + '&shy;');
+                }
+                carry = (x.slice(hyphenPos - 1)).join('');
+            } else {
+                arrPos--;
+                carry = null;
+            }
+            
+            // Clean up for next line
+            span.before(span.html());
+            span.html('');
+            console.log('line %i --> %s', line, dom.html());
+            console.log('cur pos --> ' + arrPos);
+            line++;            
+        }
     }
+    console.log('pos %i -> value %s', arrPos, remain[arrPos]);
 
-    if (hyphenPos == 1) {
-      check.html(temp);
-    } else {
-      check.html(temp + '&shy;');
-    }
-    var rem = (x.slice(hyphenPos - 1)).join('');
-    //var arr2 = arr;
-    return {
-      html: check.html(),
-      arrPos: arrPos,
-      carry: rem
-    };
-  }
-  return {
-      html: check.html(),
-      arrPos: arrPos,
-      carry: remain[arrPos - 1]
-  };
+    console.log('dom --> ' + dom.html());
+
+  
+    var tempMax = dom.height();
+    dom.html(dom.html());
+
+    return;
 }
 
+
 function typeset(dom) {
-  dom.after('<p class="width" style="position: absolute;"><span></span></p>');
-  var check = $('.width > span');
+  //dom.after('<p class="width" style="position: absolute;"><span></span></p>');
+  //var check = $('.width > span');
+  var clone = dom.clone();
   var looper = true,
       fullHtml = '',
-      domArr = dom.html().split(" "),
+      domArr = dom.html().replace( /\n/g, " " ).replace( /\s+/g, " " ).split(" "),
       res = {
         html: domArr,
         arrPos: 0,
         carry: null
       };
   console.log('word count -->'+ domArr.length);
+  console.log(domArr);
+  
+  dom.html('Foo bar');
+  console.log('height --> ' + dom.height());
+  
+  res = rec(domArr, dom.height(), dom);
+  console.log(res);
+  
+  
+  return;
+  /*
   while(looper) {
     console.log('called --> c:' + res.carry + ' w:' + dom.width() + ' ap:' + res.arrPos);
     res = rec(domArr, res.carry, dom.width(), check, res.arrPos);
@@ -88,10 +119,23 @@ function typeset(dom) {
       looper = false; 
     }
     
-  }
+  }*/
   console.log('Done');
-  check.remove();
+  //check.remove();
   dom.html(fullHtml);
+}
+
+function getMaxWidth(dom, span, height) {
+    var count = 0,
+        maxCount = 200;
+    console.log('--> ' + dom.height() + ' - ' + height);
+    while(dom.height() <= height) {
+        count++;
+        span.css('margin-left', count + 'px');
+        if (count >= maxCount) return null;
+    }
+    span.css('margin-left', '0');
+    return (count - 1);
 }
 // https://github.com/bramstein/hypher
 // https://github.com/bramstein/hyphenation-patterns
