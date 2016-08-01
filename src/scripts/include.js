49ee067f241486ffc4f21a54d2e0f3c7de3eb95d
MGApcDev <mgapcdev@gmail.com>
2016-08-01 23:57:48 +0200

    Handle nested includes and error handling

--- src/scripts/include.js
+++ src/scripts/include.js
@@ -1,8 +1,27 @@
 function includeFiles(dom) {
-  dom.find('include').each(function (index) {
-    var elem = $(this);
-    if (elem.attr('src') != '') {
-      elem.load(elem.attr('src'));
-    }
-  }); 
+    var loading = 0;
+    dom.find('include').each(function (index) {
+        var elem = $(this);
+        if (elem.attr('src') != '') {
+            loading++;
+            elem.load(elem.attr('src'), function(data) {
+                if (data === undefined) {
+                    console.error('File: "' + elem.attr('src') + '" was not found');
+                } else {
+                    elem.after(elem.html());
+                }
+                elem.remove();
+                loading--;
+            });
+        }
+    });
+    
+    var timer = setInterval(function () {
+        if (loading <= 0) {
+            if (dom.find('include').length > 0) {
+                includeFiles(dom);
+            }
+            clearInterval(timer);
+        }
+    }, 50);
 }
