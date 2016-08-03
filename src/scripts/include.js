8a8a45afecd5cff21d884d49fb408bc20a11bece
Mathias Grundtvig Andreasen <mgapcdev@gmail.com>
2016-08-03 22:08:17 +0200

    Include html file (#10)
    * Implement jquery file load
    
    * Handle nested includes and error handling
    
    * Src as variables
    
    * Include html file from url and cleanup

---
+++ src/scripts/include.js
@@ -0,0 +1,32 @@
+/**
+ * Find include tags in dom and include their sources.
+ */
+function includeFiles(dom) {
+    var loading = 0; // Count number of active load requests
+    
+    dom.find('include').each(function (index) {
+        var elem = $(this),
+            src = elem.attr('src');
+        if (src !== undefined && src !== '') {
+            loading++;
+            elem.load(src, function(data) {
+                if (data === undefined) { // File not found
+                    console.error('File: "' + src + '" was not found');
+                } else {
+                    elem.after(elem.html());
+                }
+                elem.remove();
+                loading--;
+            });
+        }
+    });
+    
+    var timer = setInterval(function () {
+        if (loading <= 0) {
+            if (dom.find('include').length > 0) {
+                includeFiles(dom);
+            }
+            clearInterval(timer);
+        }
+    }, 50);
+}
