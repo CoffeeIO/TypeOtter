6cbf72337f8c917d0281cad91667f386ad633e84
MGApcDev <mgapcdev@gmail.com>
2016-08-06 23:51:57 +0200

    Implement cite page

--- src/scripts/bib.js
+++ src/scripts/bib.js
@@ -1,5 +1,9 @@
+/**
+ * Find and replace cite tags in dom with values from biblography.
+ */
 function handleCite(dom, bib) {
-    var map = [];
+    var map = [],
+        refMap = [];
     dom.find('a[cite=""]').each(function () {
         var elem = $(this),
             href = elem.attr('href');
@@ -26,10 +30,38 @@ function handleCite(dom, bib) {
         }
         var fullCite = citeStr + citeSpace + map[citeStr];
         elem.html(fullCite);
-        console.log(fullCite);
+        refMap[fullCite] = href;
     });
+    return refMap;
 }
 
-function makeRefPage(dom, cite) {
+function getMapKeys(dictionary) {
+    var keys = [];
+    for (var key in dictionary) {
+      if (dictionary.hasOwnProperty(key)) {
+        keys.push(key);
+      }
+    }
+    return keys;
+}
+
+/**
+ * Create references page and append to dom.
+ */
+function makeRefPage(dom, bib, ref) {
+    console.log(ref);
+    var keys = getMapKeys(ref),
+        curHtml = '<div class="tex-ref"><h1 class="tex-ref-title">References</h1>';
     
+    keys.forEach(function (item) {
+        curHtml += '<div class="tex-ref-row">';
+        curHtml += '<a name="' + ref[item] + '"></a>';
+        curHtml += '<span class="tex-ref-name">[' + item + ']</span>';
+        curHtml += '<span class="tex-ref-cite">' + bib[ref[item]].title + '</span>';
+        curHtml += '</div>';
+    });
+    
+    
+    curHtml += '</div>';
+    dom.append(curHtml);
 }
