cf889ae8a036c75edf9daf0b2d3307a48adb24a8
mgru <mgru@itu.dk>
2016-08-06 08:28:31 +0200

    Generate cite name and insert in dom

--- src/scripts/bib.js
+++ src/scripts/bib.js
@@ -1,6 +1,35 @@
 function handleCite(dom, bib) {
+    var map = [];
     dom.find('a[cite=""]').each(function () {
-        var elem = $(this);
-        
+        var elem = $(this),
+            href = elem.attr('href');
+        if (href === undefined) {
+            console.error('Cite needs "href" attribute: %s', elem.clone().wrap('<span>').parent().html());
+            return true;
+        } else {
+            href = href.substr(1); // Ignore '#'
+        }
+        var ref = bib[href];
+        if (ref === undefined) {
+            console.error('Could not find reference %s', href);
+            return true;
+        }
+        var citeStr = ref.title.substr(0, 3),
+            citeSpace = '';
+        if (map[citeStr] === undefined) {
+            map[citeStr] = 1;
+        } else {
+            map[citeStr]++;
+        }
+        if (map[citeStr] < 10) {
+            citeSpace = '0';
+        }
+        var fullCite = citeStr + citeSpace + map[citeStr];
+        elem.html(fullCite);
+        console.log(fullCite);
     });
+}
+
+function makeRefPage(dom, cite) {
+    
 }
