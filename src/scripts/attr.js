b1ce6e563734b3b6c6e4133e8669a7adbd0fd134
Mathias Grundtvig Andreasen <mgapcdev@gmail.com>
2016-08-14 04:10:33 +0200

    Attribute url (#15)
    * Implement handle of url attribute on 'a' tags
    
    * Typo

--- src/scripts/attr.js
+++ src/scripts/attr.js
@@ -2,6 +2,7 @@
  * Look for specific attributes in dom and generate elements.
  */
 function attrify(dom) {
+    handleUrl(dom);
     handleImages(dom);
     handleTitle(dom);
     handleName(dom);
@@ -71,5 +72,16 @@ function handleImgSize(dom) {
     });
     dom.find("img[height!=''][height]").each(function (index) {
         $(this).css('height', $(this).attr('height'));
+    });
+}
+
+/**
+ * Handle url attribute on 'a' tags, copy href to html and set target to blank.
+ */
+function handleUrl(dom) {
+    dom.find('a[url=""]').each(function (index) {
+        var elem = $(this),
+            href = elem.attr('href');
+        elem.html(href).attr('target', '_blank');
     });
 }
