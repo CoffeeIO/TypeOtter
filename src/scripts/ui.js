1a68bad60a3b707f120ce368188b79836dd555d9
MGApcDev <MGApcDev@gmail.com>
2016-09-02 14:27:21 +0200

    Add docs and fix UI transition

--- src/scripts/ui.js
+++ src/scripts/ui.js
@@ -1,7 +1,22 @@
+/**
+ * Add UI controls to the dom.
+ */
 function uiControls(dom) {
-  dom.append('<div class="tex-ui-controls"><button class="tex-print">Print</button></div>');
+    var curHtml = '<div class="tex-ui-controls">';
+    // Print button
+    curHtml += '<button class="tex-print">Print</button>';
 
-  dom.find('.tex-print').on('click', function (){
-    window.print();
-  });
+    curHtml += '</div>';
+    dom.append(curHtml);
+
+    handlePrint(dom);
+}
+
+/**
+ * Handle click event for print button.
+ */
+function handlePrint(dom) {
+    dom.find('.tex-print').on('click', function (){
+        window.print();
+    });
 }
