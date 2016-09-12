51ab30dd25cb6311db02342e42b44d9ce7cb5256
MGApcDev <mgapcdev@gmail.com>
2016-09-13 00:30:07 +0200

    Fix main.js indent

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,48 +1,48 @@
-var mathDone = false;
-var dom = null;
-var start = null,
+var mathDone = false,
+    dom = null,
+    start = null,
     end = null;
+
 $(document).ready(function () {
-  start = new Date().getTime();
-  dom = $('body')
-  includeFiles(dom);
-  handleMath(dom);
-  MathJax.Hub.Queue(["Typeset",MathJax.Hub]); // Queue 'typeset' action
+    start = new Date().getTime(); // Debug time
+    dom = $('body');
+    includeFiles(dom);
+    handleMath(dom);
+
+    MathJax.Hub.Queue(["Typeset",MathJax.Hub]); // Queue 'typeset' action
 });
 
 $(window).load(function () {
-  // User specified options, example
-  var options = {
-      headerRight: 'MGApcDev',
-      headerCenter: '<i>by</i>',
-      pager : 'Page [cur] of [total]',
-      padding: "7mm 10mm"
-  };
+    // User specified options, example
+    var options = {
+        headerRight: 'MGApcDev',
+        headerCenter: '<i>by</i>',
+        pager : 'Page [cur] of [total]',
+        padding: "7mm 10mm"
+    };
 
+    var innerDone = false;
+    var timer = setInterval(function () { // Block until math is loaded
+        if (mathDone) {
+            setTimeout(function(){
+                innerDone = true;
+            }, 100);
+            if (innerDone) {
+                attrify(dom);
+                wrapper(dom);
+                indexToc(dom);
+                makeToc(dom);
+                makeRef(dom);
+                makeRefPage(dom, null);
+                fillMath(dom);
+                texify(options, dom);
+                fillToc(dom);
+                fillRef(dom);
+                end = new Date().getTime(); // Debug time
+                console.log(end - start); // Debug time
 
-  var innerDone = false;
-  var timer = setInterval(function () { // Block until math is loaded
-      if (mathDone) {
-          setTimeout(function(){
-            innerDone = true;
-          }, 100);
-          if (innerDone) {
-            attrify(dom);
-            wrapper(dom);
-
-            indexToc(dom);
-            makeToc(dom);
-            makeRef(dom);
-            makeRefPage(dom, null);
-          fillMath(dom);
-          texify(options, dom);
-          fillToc(dom);
-          fillRef(dom);
-          end = new Date().getTime();
-          console.log(end - start);
-          clearInterval(timer);
+                clearInterval(timer);
+            }
         }
-      }
-  }, 100);
-
+    }, 100);
 });
