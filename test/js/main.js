c4c91f622c37dd604ef61a189e5c47b57ce10149
MGApcDev <mgapcdev@gmail.com>
2016-09-16 01:22:31 +0200

    Add main example file

---
+++ test/js/main.js
@@ -0,0 +1,55 @@
+var mathDone = false,
+    dom = null,
+    DEBUG = true,
+    innerDone = false;
+
+
+$(document).ready(function () {
+    if (DEBUG) console.time("document prepare"); // Performance timers
+    if (DEBUG) console.time("document render");  // Performance timers
+    if (DEBUG) console.time("document math done"); // Performance timers
+    if (DEBUG) console.time("window loaded"); // Performance timers
+    dom = $('body');
+    includeFiles(dom);
+    handleMath(dom);
+
+    MathJax.Hub.Queue(["Typeset",MathJax.Hub]); // Queue 'typeset' action
+});
+
+$(window).load(function () {
+    if (DEBUG) console.timeEnd("window loaded"); // Performance timers
+    // User specified options, example
+    var options = {
+        
+    };
+
+    var timer = setInterval(function () { // Block until math is loaded
+        if (mathDone) {
+            var mp = dom.find('.MathJax_Preview').length,     // MathJax equations detected
+                m = dom.find('.MathJax').length,              // MathJax equations prepared
+                mpr = dom.find('.MathJax_Processing').length; // MathJax equations being processed
+            if (mp === m && mpr === 0) {
+                innerDone = true;
+            }
+
+            if (innerDone) {
+                if (DEBUG) console.timeEnd("document math done"); // Performance timers
+                clearInterval(timer);
+                setTimeout(function(){
+                    attrify(dom);
+                    wrapper(dom);
+                    indexToc(dom);
+                    makeToc(dom);
+                    makeRef(dom);
+                    makeRefPage(dom, null);
+                    fillMath(dom);
+                    if (DEBUG) console.timeEnd("document prepare"); // Performance timers
+                    texify(options, dom);
+                    fillToc(dom);
+                    fillRef(dom);
+                    if (DEBUG) console.timeEnd("document render");  // Performance timers
+                }, 100);
+            }
+        }
+    }, 100);
+});
