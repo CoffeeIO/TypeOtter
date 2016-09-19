de00fd3523b2f7c720db3f707396aebafde89179
MGApcDev <mgapcdev@gmail.com>
2016-09-19 22:13:43 +0200

    Implement spinner

---
+++ src/scripts/spinner.js
@@ -0,0 +1,25 @@
+/**
+ * Add spinner after dom element.
+ */
+function addSpinner(dom) {
+  var curHtml = '<div class="overlay tex-spinner"></div>'
+              + '<div class="bookshelf_wrapper tex-spinner" style="display:block;">'
+                + '<ul class="books_list">'
+                  + '<li class="book_item first"></li>'
+                  + '<li class="book_item second"></li>'
+                  + '<li class="book_item third"></li>'
+                  + '<li class="book_item fourth"></li>'
+                  + '<li class="book_item fifth"></li>'
+                  + '<li class="book_item sixth"></li>'
+                + '</ul>'
+                + '<div class="shelf"></div>'
+              + '</div>';
+  dom.after(curHtml);
+}
+
+/**
+ * Remove spinner from the dom.
+ */
+function removeSpinner() {
+  $('.tex-spinner').remove();
+}
