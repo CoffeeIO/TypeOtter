25718b66876f7dd649449ac51d9097f07a3cffa1
MGApcDev <mgapcdev@gmail.com>
2016-10-06 23:09:38 +0200

    Minify js files and add local jquery

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,37 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+var pump = require('pump');
+var concat = require('gulp-concat');
 
-gulp.task('default', ['sass'], function () {
+gulp.task('default', ['sass', 'uglify'], function () {
 
 });
 
 gulp.task('sass', function () {
     return gulp.src('./src/styles/source/main.scss')
         .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(concat('main.min.css'))
         .pipe(gulp.dest('./src/styles/build'));
 });
 
 gulp.task('sass:watch', function () {
     gulp.watch('./src/styles/source/*.scss', ['sass']);
+});
+
+gulp.task('uglify', function() {
+    return gulp.src([ 'src/scripts/source/attr.js',
+                      'src/scripts/source/bib.js',
+                      'src/scripts/source/include.js',
+                      'src/scripts/source/math.js',
+                      'src/scripts/source/page.js',
+                      'src/scripts/source/ref.js',
+                      'src/scripts/source/spinner.js',
+                      'src/scripts/source/toc.js',
+                      'src/scripts/source/wrap.js',
+                      'src/scripts/source/main.js'
+                    ])
+    .pipe(concat('main.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('src/scripts/build'));
 });
