fee810c39789d55b8c06d83b4563def895eeb01f
MGApcDev <mgapcdev@gmail.com>
2016-10-14 21:57:51 +0200

    Multiple test

--- test/app.test.js
+++ test/app.test.js
@@ -2,8 +2,13 @@
 
 describe('Main simple test', function () {
 
-  it('should be equal to 3', function () {
-    expect(3).toEqual(2);
-  });
+    it('should be equal to 3', function () {
+        expect(3).toEqual(3);
+    });
+
+    // Multiple tests
+    it('should be equal to 3', function () {
+        expect(4).toEqual(4);
+    });
 
 });
