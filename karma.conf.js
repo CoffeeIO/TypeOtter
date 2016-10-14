8de0251f475ab994bb699e7ec51508936913f3e4
MGApcDev <mgapcdev@gmail.com>
2016-10-15 00:27:14 +0200

    Test

--- karma.conf.js
+++ karma.conf.js
@@ -18,7 +18,7 @@ module.exports = function(config) {
             // dependencies
             {pattern: 'src/scripts/build/*.js', watched: true, included: true, served: true},
             {pattern: 'src/styles/build/*.css', watched: true, included: true, served: true},
-            {pattern: 'test/app.test.js', watched: true, included: true, served: true},
+            {pattern: 'test/app.test.js', watched: true, included: true, served: false},
             {pattern: 'test/app.html', watched: false, included: false, served: true},
 
             // tests
