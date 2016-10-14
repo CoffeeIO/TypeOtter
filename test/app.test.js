4c5cc1ba8b81c8952d54e0388578d44f30b1d01c
MGApcDev <mgapcdev@gmail.com>
2016-10-14 22:31:29 +0200

    render height

--- test/app.test.js
+++ test/app.test.js
@@ -5,7 +5,7 @@ describe('Main simple test', function () {
     it('should be equal to 3', function () {
         expect(3).toEqual(3);
     });
-    $('body').append('<div class="box"></div>');
+    $('body').append('<div class="box">test</div>');
     // Multiple tests
     it('jQuery dom test', function () {
         expect($('.box').length).toEqual(1);
@@ -16,4 +16,7 @@ describe('Main simple test', function () {
         expect($('.unknown').length).toEqual(0);
     });
 
+    it('jQuery dom height test', function () {
+        expect($('.box').height()).toEqual(16);
+    });
 });
