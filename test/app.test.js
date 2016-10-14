34fe4588c8e34b8142f6be7ca734ca11517a97da
MGApcDev <mgapcdev@gmail.com>
2016-10-14 22:21:11 +0200

    Testing

--- test/app.test.js
+++ test/app.test.js
@@ -5,7 +5,7 @@ describe('Main simple test', function () {
     it('should be equal to 3', function () {
         expect(3).toEqual(3);
     });
-
+    $('body').append('<div class="box"></div>');
     // Multiple tests
     it('jQuery dom test', function () {
         expect($('.box').length).toEqual(1);
