ae39578edb11d2d127698ce0724adbf41e95923e
MGApcDev <mgapcdev@gmail.com>
2016-11-29 00:29:08 +0100

    Adjust wrapper implementation

--- test/wrap.test.js
+++ test/wrap.test.js
@@ -2,6 +2,7 @@
 
 describe('Text wrap testing:', function () {
     var run = false;
+    var elem;
     jasmine.DEFAULT_TIMEOUT_INTERVAL = 20000;
 
     beforeEach(function(done) {
@@ -13,25 +14,29 @@ describe('Text wrap testing:', function () {
 
             mlTex.wrapper($('.unit-texting'));
             run = true;
+            elem = $('.sec1-2');
             done();
         }
     });
 
     describe('Div wrap:', function () {
         it('Before', function () {
-            expect($('.sec1-2 > p:nth-of-type(1)').html().trim()).toEqual('paragraph1');
+            expect(elem.find('> p:nth-of-type(1)').html().trim()).toEqual('paragraph1');
         });
 
         it('Middle', function () {
-            expect($('.sec1-2 > p:nth-of-type(2)').html().trim()).toEqual('paragraph4');
+            expect(elem.find('> p:nth-of-type(2)').html().trim()).toEqual('paragraph4');
         });
 
         it('After', function () {
-            expect($('.sec1-2 > p:nth-of-type(3)').html().trim()).toEqual('paragraph6');
+            expect(elem.find('> p:nth-of-type(3)').html().trim()).toEqual('paragraph6');
         });
 
         it('Nested', function () {
-            expect($('.sec1-2 > div > p:nth-of-type(1)').html().trim()).toEqual('paragraph2');
+            expect(elem.find('> div > p:nth-of-type(1)').html().trim()).toEqual('paragraph2');
+        });
+        it('Simple div with text', function () {
+            expect(elem.find('> div:nth-of-type(2) > p').html().trim()).toEqual('paragraph5');
         });
     });
 
