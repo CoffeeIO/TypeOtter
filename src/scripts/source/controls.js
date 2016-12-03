41e08abd4422c36c71602df0b97f3f6229c55047
MGApcDev <mgapcdev@gmail.com>
2016-12-03 14:45:02 +0100

    Implement zoom for chrome

--- src/scripts/source/controls.js
+++ src/scripts/source/controls.js
@@ -6,11 +6,11 @@ var mlTex = (function(obj, $) {
     obj.addControls = function (dom) {
         dom.prepend(
             '<div class="tex-controls">' +
-            '<div class="tex-icons">' +
-            '<div class="tex-control-zoomin"><i>&#xf068;</i></div>' +
-            '<div class="tex-control-zoomout"><i>&#xf067;</i></div>' +
-            '<div class="tex-control-print"><i>&#xf02f;</i></div>' +
-            '</div>' +
+                '<div class="tex-icons">' +
+                    '<div class="tex-control-zoomin"><i>&#xf068;</i></div>' +
+                    '<div class="tex-control-zoomout"><i>&#xf067;</i></div>' +
+                    '<div class="tex-control-print"><i>&#xf02f;</i></div>' +
+                '</div>' +
             '</div>' +
             '<div class="tex-hover"></div>'
         );
@@ -27,10 +27,14 @@ var mlTex = (function(obj, $) {
             window.print();
         });
         $('.tex-control-zoomin').click(function () {
-
+            var elem = $(this);
+            var doc = elem.closest('.tex-controls').parent().find('.tex-document');
+            doc.css('zoom', doc.css('zoom') * 0.9); // 10% decrease
         });
         $('.tex-control-zoomout').click(function () {
-            
+            var elem = $(this);
+            var doc = elem.closest('.tex-controls').parent().find('.tex-document');
+            doc.css('zoom', doc.css('zoom') * 1.1); // 10% increase
         });
     };
 
