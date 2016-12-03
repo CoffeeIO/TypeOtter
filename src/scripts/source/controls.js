874b2b1e5af211b9851a71cbdda4043cb01b1af2
MGApcDev <mgapcdev@gmail.com>
2016-12-03 14:18:48 +0100

    Finish print implementation and switch to html entity icons

--- src/scripts/source/controls.js
+++ src/scripts/source/controls.js
@@ -1,34 +1,36 @@
 var mlTex = (function(obj, $) {
 
     /**
-     *
-     */
+    *
+    */
     obj.addControls = function (dom) {
         dom.prepend(
             '<div class="tex-controls">' +
-                '<div>' +
-                    '<div class="zoom-in"><i class="fa fa-minus" aria-hidden="true"></i></div>' +
-                    '<div class="zoom-out"><i class="fa fa-plus" aria-hidden="true"></i></div>' +
-                    '<div class="print"><i class="fa fa-print" aria-hidden="true"></i></div>' +
-                '</div>' +
+            '<div class="tex-icons">' +
+            '<div class="tex-control-zoomin"><i>&#xf068;</i></div>' +
+            '<div class="tex-control-zoomout"><i>&#xf067;</i></div>' +
+            '<div class="tex-control-print"><i>&#xf02f;</i></div>' +
+            '</div>' +
             '</div>' +
             '<div class="tex-hover"></div>'
         );
 
-        $(".tex-hover")
-          .mouseenter(function() {
+        $(".tex-hover").mouseenter(function() {
             $('.tex-controls').addClass('show');
-            console.log('enter controls');
-          });
+        });
 
-        $(".tex-document")
-          .mouseenter(function() {
+        $(".tex-document").mouseenter(function() {
             $('.tex-controls').removeClass('show');
-            console.log('enter dom');
-          });
+        });
 
-        $('.print').click(function () {
+        $('.tex-control-print').click(function () {
             window.print();
+        });
+        $('.tex-control-zoomin').click(function () {
+
+        });
+        $('.tex-control-zoomout').click(function () {
+            
         });
     };
 
