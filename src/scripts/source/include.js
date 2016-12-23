4e54fadec66eb7765c3e945d3aa6aca0ef22717b
MGApcDev <mgapcdev@gmail.com>
2016-12-23 02:11:08 +0100

    Fix implementation and detect recursive includes

--- src/scripts/source/include.js
+++ src/scripts/source/include.js
@@ -1,36 +1,46 @@
 var mlTex = (function(obj, $) {
 
+    // @see http://werxltd.com/wp/2010/05/13/javascript-implementation-of-javas-string-hashcode-method/
+    function hashCode(str) {
+    	var hash = 0;
+    	if (str.length == 0) return hash;
+    	for (i = 0; i < str.length; i++) {
+    		char = str.charCodeAt(i);
+    		hash = ((hash<<5)-hash)+char;
+    		hash = hash & hash; // Convert to 32bit integer
+    	}
+    	return hash;
+    }
+
     /**
      * Find include tags in dom and include their sources.
      */
-    obj.includeFiles = function(dom) {
-        var loading = 0; // Count number of active load requests
-
-        dom.find('a[include=""]').each(function (index) {
+    obj.includeFiles = function(dom, map) {
+        dom.find('a[include=""]').each(function () {
             var elem = $(this),
                 href = elem.attr('href');
             if (href !== undefined && href !== '') {
-                loading++;
                 elem.load(href, function(data) {
                     if (data === undefined) { // File not found
                         console.error('File: "%s" was not found', href);
                     } else {
-                        elem.after(elem.html());
+                        var hash = hashCode(elem.html());
+                        if (map[hash] === true) {
+                            console.error('File: "%s" recursive include detected, abort abort!', href);
+
+                            elem.html(''); // Empty element
+                            console.log(elem.parent().html());
+
+                            return;
+                        }
+                        var copy = $.extend(true, {}, map);
+                        copy[hash] = true;
+                        obj.includeFiles(elem, copy); // Check included file for other includes.
+                        elem.contents().unwrap(); // Unwrap <a> from content
                     }
-                    elem.remove();
-                    loading--;
                 });
             }
         });
-
-        var timer = setInterval(function () {
-            if (loading <= 0) {
-                if (dom.find('a[include=""]').length > 0) {
-                    obj.includeFiles(dom);
-                }
-                clearInterval(timer);
-            }
-        }, 50);
     };
 
     return obj;
